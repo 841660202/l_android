ec251389d262ff1d7bf652adc93a650c35ad4b79
hailong.chen <hailong.chen@weimeng-inc.com>
2018-06-17 17:33:21 +0800

    :sparkles appBadge picker  pic

---
+++ app/src/main/java/com/victor/coordinatorlayoutdemo/activity/AppBadgeActivity.java
@@ -0,0 +1,150 @@
+package com.victor.coordinatorlayoutdemo.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.victor.coordinatorlayoutdemo.R;
+import com.victor.coordinatorlayoutdemo.Service.BadgeIntentService;
+
+import java.io.IOException;
+
+import me.leolin.shortcutbadger.ShortcutBadger;
+
+public class AppBadgeActivity extends Activity {
+    private MediaPlayer mediaPlayer;
+    private boolean playBeep;
+    private static final boolean PLAY_BEEP = true;
+    private static final boolean VIBRATE = false;
+    private static final long VIBRATE_DURATION = 200L;
+    private static final float BEEP_VOLUME = 0.1f;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_badge);
+
+
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        playBeep = PLAY_BEEP && (audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL);
+        initBeepSound();
+        init();
+
+    }
+
+    private void init() {
+
+        final EditText numInput = (EditText) findViewById(R.id.numInput);
+
+        Button button = (Button) findViewById(R.id.btnSetBadge);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int badgeCount = 0;
+                try {
+                    badgeCount = Integer.parseInt(numInput.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Error input", Toast.LENGTH_SHORT).show();
+                }
+
+                boolean success = ShortcutBadger.applyCount(AppBadgeActivity.this, badgeCount);
+
+                Toast.makeText(getApplicationContext(), "Set count=" + badgeCount + ", success=" + success, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button launchNotification = (Button) findViewById(R.id.btnSetBadgeByNotification);
+        launchNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int badgeCount = 0;
+                try {
+                    badgeCount = Integer.parseInt(numInput.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Error input", Toast.LENGTH_SHORT).show();
+                }
+
+                playBeepSoundAndVibrate();
+                startService(
+                        new Intent(AppBadgeActivity.this, BadgeIntentService.class).putExtra("badgeCount", badgeCount)
+                );
+            }
+        });
+
+        Button removeBadgeBtn = (Button) findViewById(R.id.btnRemoveBadge);
+        removeBadgeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean success = ShortcutBadger.removeCount(AppBadgeActivity.this);
+
+                Toast.makeText(getApplicationContext(), "success=" + success, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //find the home launcher Package
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        ResolveInfo resolveInfo = getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        String currentHomePackage = resolveInfo.activityInfo.packageName;
+
+        TextView textViewHomePackage = (TextView) findViewById(R.id.textViewHomePackage);
+        textViewHomePackage.setText("launcher:" + currentHomePackage);
+    }
+
+
+
+    private void initBeepSound() {
+        if (playBeep && mediaPlayer == null) {
+            // The volume on STREAM_SYSTEM is not adjustable, and users found it too loud,
+            // so we now play on the music stream.
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setOnCompletionListener(beepListener);
+
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.dingdang);
+            try {
+                mediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+                file.close();
+                mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                mediaPlayer = null;
+            }
+        }
+    }
+
+    private void playBeepSoundAndVibrate() {
+        if (playBeep && mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+        if (VIBRATE) {
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            vibrator.vibrate(VIBRATE_DURATION);
+        }
+    }
+
+
+
+
+    /**
+     * When the beep has finished playing, rewind to queue up another one.
+     */
+    private final MediaPlayer.OnCompletionListener beepListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
+}
